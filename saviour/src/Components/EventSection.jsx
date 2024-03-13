3080fe94b36013c9e814ba9272bc303051b7baf4
Parmeshwar Murmu <murmuparmeshwar05@gmail.com>
2024-03-13 16:16:29 +0530

    added_button_inputs

--- saviour/src/Components/EventSection.jsx
+++ saviour/src/Components/EventSection.jsx
@@ -13,12 +13,13 @@ export const EventSection = () => {
             <p className='z-50 opacity-100 w-539 h-111 absolute top-1385 left-80 font-zcool text-21 leading-36 text-center font-normal text-eventText'>During our IDO event, you will gain early access to our revolutionary ecosystem, designed to empower everyone to share wealth and achieve success.</p>
 
             <div className='z-50 w-244 h-70 absolute top-1520 left-229 rounded-20 bg-red opacity-100'>
-                <p className='text-white w-125 h-24 mt-23 ml-60 mr-60 mb-23 font-shojumaru text-18 leading-23 font-normal'>Token Info</p>
+                <p className='text-white z-50 w-125 h-24 mt-23 ml-60 mr-60 mb-23 font-shojumaru text-18 leading-23 font-normal'>Token Info</p>
 
             </div>
             {/* border-gradient-to-r from-[#ED0137] via-transparent to-[#F05733] rounded-20 relative */}
-            <div className='opacity-100 w-412 h-382 absolute top-1562 left-151 border-1 rounded-20 border-red-400'>
-                <div className='flex border-4 border-amber-300'>
+            <div className='opacity-100 w-412 h-382 absolute top-1562 left-151 border-1 rounded-20 border-gradient-to-r from-[#ED0137] via-transparent to-[#F05733]'>
+                
+                <div className='z-50 flex border-4 border-amber-300'>
                     <p className='text-white w-176 h-40 border-4 mt-64  ml-59 top-1626 left-210 font-zcool leading-20 text-19 font-normal'>Total Token Supply</p>
                     <p className='text-eventPercent w-39 h-20 mt-62 ml-15 font-zcool text-19 leading-20 font-normal'>20%</p>
                 </div>
@@ -46,6 +47,18 @@ export const EventSection = () => {
                     <p className='text-white w-152 h-20 mt-26 ml-59 leading-21 text-19 font-zcool font-normal'>2nd Generation</p>
                     <p className='text-eventPercent h-20 w-26 ml-26 mt-26 leading-21 text-19 font-zcool font-normal'>2%</p>
                 </div>
+                
+            </div>
+
+            <div className='z-50 h-39 w-138 rounded-16  absolute top-1981 left-286 bg-gradient-to-r from-[#ED0137] via-transparent to-[#F05733]'>
+                <button className='text-white mt-12 ml-15 mb-12 mr-15 font-zcool text-14 leading-14 w-107 h-14'>Connect Wallet</button>
+            </div>
+
+            <p className='text-red z-50 text-center w-547 h-74 absolute top-2056 left-72 font-shojumaru font-normal text-28 leading-37 '>Become an affiliate & Earn 7% as Commission!</p>
+
+            <div className='text-white z-50 absolute top-2160 left-111 w-467 h-48  rounded-20 border-1 border-red'>
+                <input type="text" placeholder='Generate a unique referral link' className='placeholderColor bg-black mt-16 ml-20 mr-106 mb-16 h-16 w-239 font-normal font-zcool text-16 leading-16' />
+                <button className='w-100 h-42 rounded-20 bg-red font-zcool text-white'>Generate</button>
             </div>
 
 
@@ -57,16 +70,16 @@ export const EventSection = () => {
                 <p className='text-presaleText2 font-zcool ml-120 mt-40 w-467 h-24 text-24 leading-24 font-normal'>Raised - $34,56,56,764/$40,00,00,000</p>
                 <div>
                     <div >
-                        <input type='text' placeholder='Enter the amount (BNB)' className='w-467 h-48 ml-120 mt-40 rounded-20 border-5 text-inputBorder font-zcool bg-black
-                pt-4 pb-4 pl-5'
+                        <input type='text' placeholder='Enter the amount (BNB)' className='w-467 h-48 ml-120 mt-40 rounded-20 border-5 border-inputBorder font-zcool bg-black
+                pt-4 pb-4 pl-5 text-placeholderColor'
                         />
 
-                        <input type='text' placeholder='Minimum Quantity to Buy' className='w-467 h-48 ml-120 mt-19 rounded-20 border-5 text-inputBorder font-zcool bg-black
-                pt-4 pb-4 pl-5'
+                        <input type='text' placeholder='Minimum Quantity to Buy' className='w-467 h-48 ml-120 mt-19 rounded-20 border-5 border-inputBorder font-zcool bg-black
+                pt-4 pb-4 pl-5 text-placeholderColor'
                         />
 
-                        <input type='text' placeholder='Maximum Quantity of Tokens' className='w-467 h-48 ml-120 mt-19 rounded-20 border-5 text-inputBorder font-zcool bg-black
-                pt-4 pb-4 pl-5'
+                        <input type='text' placeholder='Maximum Quantity of Tokens' className='w-467 h-48 ml-120 mt-19 rounded-20 border-5 border-inputBorder font-zcool bg-black
+                pt-4 pb-4 pl-5 text-placeholderColor'
                         />
                     </div>
                 </div>
